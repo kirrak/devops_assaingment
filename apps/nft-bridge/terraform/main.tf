a47b0ca6cf2d0a269f3a773173671f50208df4ca
kirrak <karthik0248@gmail.com>
2024-12-05 21:47:17 +0530

    added .tf

--- apps/nft-bridge/terraform/main.tf
+++ apps/nft-bridge/terraform/main.tf
@@ -82,7 +82,7 @@ resource "aws_iam_role_policy_attachment" "registry_read_policy" {
   role       = aws_iam_role.node_role.name
 }
 
-# CloudWatch Log Group for EKS Cluster
+# CloudWatch Log Group for EKS cluster (this should be unique)
 resource "aws_cloudwatch_log_group" "this" {
   name = "/aws/eks/default-eks-cluster/cluster"
 }
