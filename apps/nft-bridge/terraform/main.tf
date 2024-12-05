fa31636cf545f56cabf216ba5944ed997c34440f
kirrak <karthik0248@gmail.com>
2024-12-05 19:35:43 +0530

    added changes

--- apps/nft-bridge/terraform/main.tf
+++ apps/nft-bridge/terraform/main.tf
@@ -2,105 +2,33 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Data sources for default VPC and subnets
+# Fetch the default VPC
 data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
-}
-
-data "aws_security_group" "default" {
-  vpc_id = data.aws_vpc.default.id
-  tags = {
-    Name = "default"
+# Fetch all subnets in the default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
 }
 
-# EKS Cluster IAM Role
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks-cluster-role"
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = var.cluster_name
+  cluster_version = "1.26"
+  subnets         = data.aws_subnets.default.ids
+  vpc_id          = data.aws_vpc.default.id
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-        Effect = "Allow"
-        Sid    = ""
-      }
-    ]
-  })
-}
+  node_groups = {
+    eks_nodes = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
 
-resource "aws_iam_role_policy_attachment" "eks_cluster_policies" {
-  for_each = toset([
-    "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
-    "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  ])
-  policy_arn = each.value
-  role       = aws_iam_role.eks_cluster_role.name
-}
-
-# EKS Cluster
-resource "aws_eks_cluster" "eks_cluster" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster_role.arn
-
-  vpc_config {
-    subnet_ids         = data.aws_subnet_ids.default.ids
-    security_group_ids = [data.aws_security_group.default.id]
+      instance_types = ["t3.medium"]
+    }
   }
-
-  depends_on = [aws_iam_role_policy_attachment.eks_cluster_policies]
-}
-
-# EKS Node Group IAM Role
-resource "aws_iam_role" "eks_node_group_role" {
-  name = "eks-node-group-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-        Effect = "Allow"
-        Sid    = ""
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_node_group_policies" {
-  for_each = toset([
-    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-    "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
-  ])
-  policy_arn = each.value
-  role       = aws_iam_role.eks_node_group_role.name
-}
-
-# EKS Node Group
-resource "aws_eks_node_group" "eks_node_group" {
-  cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_group_name = "eks-node-group"
-  node_role_arn   = aws_iam_role.eks_node_group_role.arn
-
-  subnet_ids = data.aws_subnet_ids.default.ids
-
-  scaling_config {
-    desired_size = var.node_group_desired_size
-    max_size     = var.node_group_max_size
-    min_size     = var.node_group_min_size
-  }
-
-  depends_on = [aws_iam_role_policy_attachment.eks_node_group_policies]
 }
