26fb69c53be6df3537ed03d6a297186772c4d56f
kirrak <karthik0248@gmail.com>
2024-12-05 19:55:42 +0530

    added changes

--- apps/nft-bridge/terraform/outputs.tf
+++ apps/nft-bridge/terraform/outputs.tf
@@ -1,14 +1,11 @@
 output "eks_cluster_name" {
-  value = aws_eks_cluster.eks_cluster.name
-  description = "Name of the EKS cluster."
+  value = module.eks.cluster_id
 }
 
 output "eks_cluster_endpoint" {
-  value = aws_eks_cluster.eks_cluster.endpoint
-  description = "Endpoint of the EKS cluster."
+  value = module.eks.cluster_endpoint
 }
 
-output "eks_cluster_certificate_authority" {
-  value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
-  description = "Certificate Authority data for the EKS cluster."
+output "eks_cluster_security_group_id" {
+  value = module.eks.cluster_security_group_id
 }
--- apps/nft-bridge/terraform/variables.tf
+++ apps/nft-bridge/terraform/variables.tf
@@ -1,24 +1,9 @@
 variable "aws_region" {
-  default = "ap-south-1"
-  description = "AWS region to deploy resources."
+  description = "AWS region"
+  default     = "ap-south-1"
 }
 
 variable "cluster_name" {
-  default = "nft-bridge-eks-cluster"
-  description = "Name of the EKS cluster."
-}
-
-variable "node_group_desired_size" {
-  default = 2
-  description = "Desired number of worker nodes in the node group."
-}
-
-variable "node_group_max_size" {
-  default = 3
-  description = "Maximum number of worker nodes in the node group."
-}
-
-variable "node_group_min_size" {
-  default = 1
-  description = "Minimum number of worker nodes in the node group."
+  description = "EKS Cluster name"
+  default     = "default-eks-cluster"
 }
