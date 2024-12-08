6a9121148997f6a744e82a5903eb05fb74eb66f6
kirrak <karthik0248@gmail.com>
2024-12-08 21:31:59 +0530

    added modules changes

--- apps/nft-bridge/modules/eks/main.tf
+++ apps/nft-bridge/modules/eks/main.tf
@@ -36,7 +36,7 @@ Content-Type: multipart/mixed; boundary="==MYBOUNDARY=="
 --==MYBOUNDARY==
 Content-Type: text/x-shellscript; charset="us-ascii"
 #!/bin/bash
-/etc/eks/bootstrap.sh prod-cluster
+/etc/eks/bootstrap.sh ${var.cluster_name}
 --==MYBOUNDARY==--\
   EOF
 )
