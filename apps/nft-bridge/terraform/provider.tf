550ecce913c5191940f80b83dc7033c6b17fd5d8
kirrak <karthik0248@gmail.com>
2024-12-08 19:41:56 +0530

    added modules changes

--- apps/nft-bridge/terraform/provider.tf
+++ apps/nft-bridge/terraform/provider.tf
@@ -9,6 +9,5 @@ terraform {
     bucket         = "terraformstate-01"
     key            = "aws-eks-terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform_state"
   }
 }
